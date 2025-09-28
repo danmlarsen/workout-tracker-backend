0177f21509b3567576c35ee2bdbb078f2e258ab1
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-09-28 10:37:14 +0200

    feat: add pino-http logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,20 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExercisesModule } from './exercises/exercises.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty' }
+            : undefined,
+      },
+    }),
     PrismaModule,
     WorkoutsModule,
     UsersModule,
