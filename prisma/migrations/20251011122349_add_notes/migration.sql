566ea2fb08b8852da2dee3368d56d288df5f9987
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-10-11 17:20:02 +0200

    feat: add note fields for workouts, workoutExercises and workoutSets

---
+++ prisma/migrations/20251011122349_add_notes/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "public"."Workout" ADD COLUMN     "notes" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."WorkoutExercise" ADD COLUMN     "notes" TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."WorkoutSet" ADD COLUMN     "notes" TEXT;
