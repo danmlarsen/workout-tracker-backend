217cff4db1c614fa43aad10ec90ace684c195c8b
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-10-28 19:06:04 +0100

    feat: integrate SendGrid for sending confirmation emails

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import sgMail from '@sendgrid/mail';
 
 @Injectable()
 export class EmailService {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) {
+    const apikey: string = this.configService.getOrThrow('SENDGRID_API_KEY');
+    sgMail.setApiKey(apikey);
+  }
 
   async sendConfirmationEmail(email: string, token: string) {
     const confirmationUrl = `${this.configService.get('FRONTEND_URL')}/confirm-email?token=${token}`;
     const emailContent = {
       to: email,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      from: this.configService.getOrThrow('SENDGRID_VERIFIED_SENDER_EMAIL'),
       subject: 'Confirm your email address',
       html: `
         <h1>Welcome to Workout Tracker!</h1>
@@ -18,9 +24,6 @@ export class EmailService {
       `,
     };
 
-    await Promise.resolve();
-
-    // Send email implementation here
-    console.log('Sending confirmation email:', emailContent);
+    return await sgMail.send(emailContent);
   }
 }
