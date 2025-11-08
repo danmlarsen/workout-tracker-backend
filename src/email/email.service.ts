7bd9b0eff500c925766747038c0726cd55deb510
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-11-08 19:12:38 +0100

    fix: Correct welcome header in confirmation email template

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -29,7 +29,7 @@ export class EmailService {
       ),
       subject: 'Confirm your email address',
       html: `
-        <h1>Welcome to NextLift - Workout Tracker</h1>
+        <h1>NextLift - Workout Tracker</h1>
         <p>Please click the link below to confirm your email address:</p>
         <a href="${confirmationUrl}">Confirm Email</a>
         <p>This link will expire in 24 hours.</p>
